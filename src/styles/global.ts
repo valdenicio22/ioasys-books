9238c660a4bc0987d3f74d752da1eb5207a3cb5e
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-27 20:03:59 -0300

    feat: add div portal to document and style to global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -40,5 +40,16 @@ const GlobalStyles = createGlobalStyle`
       font-weight: ${theme.font.weight.medium};
     }
   `}
+  .react-modal-overlay {
+      background-color: rgba(0, 0, 0, 0.5);
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
 `
 export default GlobalStyles
