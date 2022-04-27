5785e8b843470a46a120ad39a9e8856450d94fa1
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-26 22:52:40 -0300

    feat: add basic styled and test page

---
+++ src/containers/LoginPage/index.tsx
@@ -0,0 +1,6 @@
+import * as S from './styles'
+const LoginPage = () => {
+  return <S.Wrapper>LoginPage</S.Wrapper>
+}
+
+export default LoginPage
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,26 @@
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <title>Ioasys Books</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        <link
+          href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
