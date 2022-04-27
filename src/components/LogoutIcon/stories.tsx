a60b9565dbe89d4b88fbb1bc7b6fc002cc9957be
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-27 15:08:28 -0300

    feat: create logoutIcon

---
+++ src/components/LogoutIcon/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import LogoutIcon from '.'
+
+export default {
+  title: 'LogoutIcon',
+  component: LogoutIcon
+} as Meta
+
+export const Default: Story = (args) => <LogoutIcon {...args} />
