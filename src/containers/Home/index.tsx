6e98b7929dfe72132f3ef6b90b5906de2a7d9801
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-27 16:57:05 -0300

    feat: map thought books

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,18 +1,30 @@
 import CardBook from 'components/CardBook'
 import Header from 'components/Header'
+import { useEffect, useState } from 'react'
+import { api } from 'service/api'
 import * as S from './styles'
+import { Book } from '../../types/types'
 
-function Home() {
+export default function Home() {
+  const [books, setBooks] = useState<Book[]>([])
+  useEffect(() => {
+    api
+      .get('/books?page=1&amount=25')
+      .then((response) => setBooks(response.data.data))
+  }, [])
+
   return (
     <S.Wrapper>
       <S.HomeContainer>
         <Header />
         <S.CardsList>
-          <CardBook />
+          {books ? (
+            books.map((book) => <CardBook book={book} key={book.id} />)
+          ) : (
+            <p>Loading</p>
+          )}
         </S.CardsList>
       </S.HomeContainer>
     </S.Wrapper>
   )
 }
-
-export default Home
