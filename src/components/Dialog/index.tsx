1caa94d133370b6eb80696dd23ef59209286cbb1
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-27 20:03:00 -0300

    feat: add modal component

---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import Modal from 'react-modal'
+import * as S from './styles'
+
+Modal.setAppElement('#modal-root')
+
+type ReactModalProps = {
+  isModalOpen: boolean
+  onCloseModal: () => void
+  children?: ReactNode
+}
+
+export const Dialog = ({
+  isModalOpen,
+  onCloseModal,
+  children
+}: ReactModalProps) => {
+  return (
+    <S.StyledModal
+      isOpen={isModalOpen}
+      onRequestClose={onCloseModal}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      {children}
+      <S.CloseBtn type="button" onClick={onCloseModal}>
+        X
+      </S.CloseBtn>
+    </S.StyledModal>
+  )
+}
