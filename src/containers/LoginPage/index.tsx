5b492ba3bf9bcbc467e13e032d1de0ed400e8a3a
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-28 13:11:21 -0300

    feat: update LoginPage to get the data from the data

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,10 +1,19 @@
 import Button from 'components/Button'
 import Logo from 'components/Logo'
 import { TextField } from 'components/Textfield'
-import { useRouter } from 'next/router'
+import { useAuth } from 'context/AuthContext'
+import { MouseEvent, useState } from 'react'
+
 import * as S from './styles'
 const LoginPage = () => {
-  const router = useRouter()
+  const { signIn } = useAuth()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSigninClick = async (e: MouseEvent) => {
+    e.preventDefault()
+    await signIn({ email, password })
+  }
 
   return (
     <S.Wrapper>
@@ -17,10 +26,18 @@ const LoginPage = () => {
             label="Email"
             labelFor="email"
             placeholder="books@ioasys.com.br"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <TextField label="Senha" type="senha" placeholder="********" />
+          <TextField
+            label="Senha"
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <S.ButtonContainer>
-            <Button type="button" onClick={() => router.push('/home')}>
+            <Button type="submit" onClick={handleSigninClick}>
               Entrar
             </Button>
           </S.ButtonContainer>
