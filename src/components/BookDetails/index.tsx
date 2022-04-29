6f2c5132b599a278970f91fc831ca21b037bb1ca
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-28 23:53:33 -0300

    feat: add BookDetails component and add style to it

--- src/components/BookDetails/index.tsx
+++ src/components/BookDetails/index.tsx
@@ -1,31 +1,88 @@
-import { Dispatch, SetStateAction, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { api } from 'service/api'
 import { Book } from 'types/types'
 import * as S from './styles'
 
 type BookDetailsProps = {
   isSelectedCard: Book['id']
-  setIsSelectedCard: Dispatch<SetStateAction<string>>
 }
 
-const BookDetails = ({
-  isSelectedCard,
-  setIsSelectedCard
-}: BookDetailsProps) => {
+const BookDetails = ({ isSelectedCard }: BookDetailsProps) => {
+  const [bookDetails, setBookDetails] = useState<Book>()
   useEffect(() => {
     try {
       api
         .get(`/books/${isSelectedCard}`)
-        .then((response) => console.log(response))
+        .then((response) => setBookDetails(response.data))
     } catch (error) {
       console.log(error)
     }
   }, [isSelectedCard])
   return (
-    <S.Wrapper>
-      {isSelectedCard}{' '}
-      <button onClick={() => setIsSelectedCard('')}>Click me</button>{' '}
-    </S.Wrapper>
+    <>
+      {!bookDetails ? (
+        <p>Carregando...</p>
+      ) : (
+        <S.Wrapper>
+          <S.BookImage>
+            <img src={bookDetails?.imageUrl} alt={bookDetails?.title} />
+          </S.BookImage>
+          <S.BookDetailsContainer>
+            <S.TitleAndAuthors>
+              <S.Title>{bookDetails?.title}</S.Title>
+              {bookDetails?.authors &&
+                bookDetails?.authors.map((author, index) =>
+                  bookDetails?.authors.length - 1 === index ? (
+                    <S.Author key={author}>{author}</S.Author>
+                  ) : (
+                    <S.Author key={author}>{author}, </S.Author>
+                  )
+                )}
+            </S.TitleAndAuthors>
+
+            <S.BookInfo>
+              <S.BookInfoTitle>Informações</S.BookInfoTitle>
+              <S.BookListInfo>
+                <S.ItemTitle>
+                  Páginas <S.ItemData>{bookDetails?.pageCount}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  Editora <S.ItemData>{bookDetails?.publisher}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  Publicação <S.ItemData>{bookDetails?.published}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  Publicação <S.ItemData>{bookDetails?.published}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  Idioma <S.ItemData>{bookDetails?.language}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  Título Original <S.ItemData>{bookDetails?.title}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  ISBN-10 <S.ItemData>{bookDetails?.isbn10}</S.ItemData>
+                </S.ItemTitle>
+                <S.ItemTitle>
+                  ISBN-13 <S.ItemData>{bookDetails?.isbn13}</S.ItemData>
+                </S.ItemTitle>
+              </S.BookListInfo>
+            </S.BookInfo>
+
+            <S.PublisherReview>
+              <S.PublisherReviewTitle>
+                RESENHA DA EDITORA
+              </S.PublisherReviewTitle>
+              <S.Review>
+                <img src="img/quotes.svg" width={17} height={14} />
+                {bookDetails?.description}
+              </S.Review>
+            </S.PublisherReview>
+          </S.BookDetailsContainer>
+        </S.Wrapper>
+      )}
+    </>
   )
 }
 
