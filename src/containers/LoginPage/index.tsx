5950cae620ff1763404e62696314239919ec11df
Valdenício Ferreira <valdenicio22@gmail.com>
2022-04-29 02:25:43 -0300

    feat: add Error component to login page

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,12 +1,13 @@
 import Button from 'components/Button'
+import Error from 'components/Error'
 import Logo from 'components/Logo'
 import { TextField } from 'components/Textfield'
 import { useAuth } from 'context/AuthContext'
-import { MouseEvent, useState } from 'react'
+import { MouseEvent, useEffect, useState } from 'react'
 
 import * as S from './styles'
 const LoginPage = () => {
-  const { signIn } = useAuth()
+  const { signIn, error, setError } = useAuth()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -14,6 +15,13 @@ const LoginPage = () => {
     e.preventDefault()
     await signIn({ email, password })
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setError('')
+    }, 3000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error])
 
   return (
     <S.Wrapper>
@@ -42,6 +50,11 @@ const LoginPage = () => {
             </Button>
           </S.ButtonContainer>
         </S.TextFieldsContainer>
+        {error && (
+          <S.ErrorContainer>
+            <Error />{' '}
+          </S.ErrorContainer>
+        )}
       </S.Form>
     </S.Wrapper>
   )
